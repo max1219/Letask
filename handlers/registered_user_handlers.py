4b2bae1840da4c55939e1724e9d814feb43d5bef
= <=>
2023-07-05 15:32:35 +0600

    Added help description in lexicon and /id command

--- handlers/registered_user_handlers.py
+++ handlers/registered_user_handlers.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.filters import Command, StateFilter
 from services import quests_answers_sender
 from data import database
-from data.database_filters import check_registered_sender_filter, check_answer_filter
+from data.database_filters import check_registered_sender_filter, check_reply_is_question_filter
 from lexicon import lexicon
 from services.question import Question
 from states.states import AskingStates
@@ -20,13 +20,18 @@ async def process_start_help(message: Message):
     await message.answer(lexicon.ANSWERS['help'])
 
 
+@router.message(Command('id'))
+async def process_get_id(message: Message):
+    await message.answer(lexicon.ANSWERS['your_id'] + str(message.from_user.id))
+
+
 @router.message(F.text == lexicon.BUTTONS['aks'])
 async def process_ask(message: Message, state: FSMContext):
     await state.set_state(AskingStates.fill_id)
     await message.answer(lexicon.ANSWERS['write_id'], reply_markup=ReplyKeyboardRemove())
 
 
-@router.message(check_answer_filter)
+@router.message(check_reply_is_question_filter)
 async def handle_answering(message: Message, question: Question):
     await quests_answers_sender.send_answer(message, question)
     await message.answer(lexicon.ANSWERS['success_answer'])
@@ -34,5 +39,4 @@ async def handle_answering(message: Message, question: Question):
 
 @router.message()
 async def handle_unknown_message(message: Message):
-
     await message.answer(lexicon.ANSWERS['unknown'])

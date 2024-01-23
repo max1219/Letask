8c21cdc1cefef178348e82b7e21d1ec62f694b2b
max1219 <bescenniy.2003@mail.ru>
2024-01-23 13:26:44 +0400

    Start of reimagination

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import asyncio
 import logging
 import sys
 
-from config_data.config import Config, load_config
-from aiogram import Bot, Dispatcher
-from handlers import user_handlers, asking_handlers
-from aiogram.fsm.storage.memory import MemoryStorage
-from middlewares.registered_checker_middleware import RegisteredCheckerMiddleware
+#from config_data.config import Config, load_config
+#from aiogram import Bot, Dispatcher
+#from handlers import user_handlers, asking_handlers
+#from aiogram.fsm.storage.memory import MemoryStorage
+#from middlewares.registered_checker_middleware import RegisteredCheckerMiddleware
 
 
 async def main() -> None:
@@ -21,19 +21,19 @@ async def main() -> None:
     )
     logging.info("Start of initialization")
 
-    config: Config = load_config()
-    storage: MemoryStorage = MemoryStorage()
+    #config: Config = load_config()
+    #storage: MemoryStorage = MemoryStorage()
 
-    bot: Bot = Bot(config.bot.token)
-    dp: Dispatcher = Dispatcher(storage=storage)
-    dp.update.outer_middleware(RegisteredCheckerMiddleware())
+    #bot: Bot = Bot(config.bot.token)
+    #dp: Dispatcher = Dispatcher(storage=storage)
+    #dp.update.outer_middleware(RegisteredCheckerMiddleware())
 
-    dp.include_router(asking_handlers.router)
-    dp.include_router(user_handlers.router)
+    #dp.include_router(asking_handlers.router)
+    #dp.include_router(user_handlers.router)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
-    logging.info("Bot started")
+    #await bot.delete_webhook(drop_pending_updates=True)
+    #await dp.start_polling(bot)
+    #logging.info("Bot started")
 
 if __name__ == '__main__':
     asyncio.run(main())

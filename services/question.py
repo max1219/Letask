72da025a1ccbe81060420164e65e6ae10167feb3
max1219 <bescenniy.2003@mail.ru>
2024-01-24 20:09:33 +0400

    Question -> dataclass. Removed some methods from Question

--- services/question.py
+++ services/question.py
@@ -1,25 +1,17 @@
+from dataclasses import dataclass
+
 from aiogram.types import Message
 
 
+@dataclass(slots=True)
 class Question:
-    __slots__ = 'text', 'questioner_message_id', 'questioner_chat_id', 'recipient_message_id', 'recipient_chat_id'
-
-    def __init__(self, text, questioner_message_id, questioner_chat_id, recipient_message_id, recipient_chat_id):
-        self.text = text
-        self.questioner_message_id = questioner_message_id
-        self.questioner_chat_id = questioner_chat_id
-        self.recipient_message_id = recipient_message_id
-        self.recipient_chat_id = recipient_chat_id
+    questioner_message_id: int
+    questioner_chat_id: int
+    recipient_message_id: int
+    recipient_chat_id: int
+    text: str
 
     @classmethod
     def from_messages(cls, questioner_message: Message, recipient_message: Message) -> "Question":
-        return cls(recipient_message.text, questioner_message.message_id, questioner_message.chat.id,
-                   recipient_message.message_id, recipient_message.chat.id)
-
-    @classmethod
-    def from_array(cls, array: list[str | int]) -> "Question":
-        return cls(*array)
-
-    def to_array(self) -> list[str | int]:
-        return [self.text, self.questioner_message_id, self.questioner_chat_id, self.recipient_message_id,
-                self.recipient_chat_id]
+        return cls(questioner_message.message_id, questioner_message.chat.id,
+                   recipient_message.message_id, recipient_message.chat.id, recipient_message.text)
